3cf91ebd86e46761fb0f97beb354cc0291382d06
Farhan Ali <farhan.ali@utexas.edu>
2016-02-20 13:38:51 -0600

    Adding a few attributes and methods to item and grocery class (more to come)

--- src/Assignment3/Grocery.java
+++ src/Assignment3/Grocery.java
@@ -2,8 +2,29 @@ package Assignment3;
 
 public class Grocery extends Item {
 	//variables, constructor here
+	private static double salesTax = 0;
 	
+	enum classification {PERISH, NONPERISH};
+	private classification perishable;
+	
+	public Grocery(String name, double price, int quantity, int weight, String Pnp) {
+		super(name, price, quantity, weight);
+		if (Pnp.equals("P")){
+			this.perishable = classification.PERISH;
+		}
+		else if(Pnp.equals("NP")){
+			this.perishable = classification.NONPERISH;
+		}
+	}
 	//override calculatePrice() if necessary; Implement print methods as necessary	
 	// Only re-implement stuff you cannot get from the superclass (Item)
+	public void calculateShipping(){
+		if (this.perishable == classification.PERISH){
+			this.shippingPrice = this.weight*20*this.quantity*1.2;
+		}
+		else{
+			this.shippingPrice = this.weight*20*this.quantity;
+		}
+	}
 	
 }
--- src/Assignment3/Item.java
+++ src/Assignment3/Item.java
@@ -3,8 +3,22 @@ package Assignment3;
 public class Item 
 {
 //Declare variables for this class. Think about its type: public, protected or private?
-
+	String name;
+	double price;
+	double shippingPrice;
+	protected double salesTax = 0.1;
+	protected int quantity;
+	int weight;
+	
+	double totalPrice; //including either premium or regular shipping and tax if applicable
+	
 // You will need a constructor (Why?). Create it here.
+	public Item(String name, double price, int quantity, int weight){
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		this.weight = weight;
+	}
 	
 	float calculatePrice () 
 	{
@@ -17,6 +31,14 @@ public class Item
 	void printItemAttributes () 
 	{
 		//Print all applicable attributes of this class
+	}
+	
+	public void calculateRegularShipping(){ //use the technique in the assignment description to calculate the shipping price (variable)
+		this.shippingPrice = this.weight*20*this.quantity;
+	}
+	
+	public void calculatePremiumShipping(){ //use the technique in the assignment description to calculate the shipping price (variable)
+		this.shippingPrice = this.weight*20*this.quantity*1.2;
 	}
 
 }
